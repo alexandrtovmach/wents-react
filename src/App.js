6b6592c9140398cf9c01d04348316f28194c252a
alexandrtovmach <alexandrtovmach@gmail.com>
2018-11-10 16:48:28 +0200

    implemented main structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-class App extends Component {
+import {
+  HomeContainer,
+  LoginContainer,
+  SupportContainer,
+  SearchRentContainer,
+  PostRentContainer,
+  ProfileContainer
+} from "./containers";
+import { PageNotFound } from './components';
+import './App.scss';
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Switch>
+        <Route
+          path="/"
+          component={HomeContainer}
+        />
+        <Route
+          path="/login"
+          component={LoginContainer}
+        />
+        <Route
+          path="/search-rent"
+          component={SearchRentContainer}
+        />
+        <Route
+          path="/post-rent"
+          component={PostRentContainer}
+        />
+        <Route
+          path="/profile"
+          component={ProfileContainer}
+        />
+        <Route
+          path="/login"
+          component={LoginContainer}
+        />
+        <Route
+          path="/support"
+          component={SupportContainer}
+        />
+        <Route component={PageNotFound} />
+      </Switch>
     );
   }
 }
