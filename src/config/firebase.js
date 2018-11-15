129e5d922b45bca58be79eaa08b1acd1a12498b9
alexandrtovmach <alexandrtovmach@gmail.com>
2018-11-15 10:10:25 +0200

    moved to another Firebase instance

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "AIzaSyCh804SwAKK3pQZpidSSHGZA51MNfmW8nA",
-  authDomain: "todolist-1asca.firebaseapp.com",
-  databaseURL: "https://todolist-1asca.firebaseio.com",
-  projectId: "todolist-1asca",
-  storageBucket: "todolist-1asca.appspot.com",
-  messagingSenderId: "180812312956"
+  apiKey: "AIzaSyAZ-KwL8IYNDbtCnnYPRYcCyFDfkl-22j4",
+  authDomain: "wents-react-26317.firebaseapp.com",
+  databaseURL: "https://wents-react-26317.firebaseio.com",
+  projectId: "wents-react-26317",
+  storageBucket: "wents-react-26317.appspot.com",
+  messagingSenderId: "364470146448"
 };
