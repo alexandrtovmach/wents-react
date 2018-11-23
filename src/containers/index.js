87884cbfb5ff89dacd1f374ef6c7a38f398ab357
alexandrtovmach <alexandrtovmach@gmail.com>
2018-11-23 17:38:16 +0200

    renamed

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,6 +2,6 @@ export { default as HomeContainer } from './Home/Home';
 export { default as SearchRentContainer } from './SearchRent/SearchRent';
 export { default as LoginContainer } from './Login/Login';
 export { default as SupportContainer } from './Home/Home';
-export { default as AdvertiseListContainer } from './AdvertiseListContainer/AdvertiseListContainer';
+export { default as AdvertiseListContainer } from './AdvertiseList/AdvertiseList';
 export { default as AdvertiseContainer } from './Advertise/Advertise';
 export { default as ProfileContainer } from './Profile/Profile';
