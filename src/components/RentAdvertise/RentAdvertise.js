e0d7d82dadb5f26bdb76c76dc9fd8f8dab83b951
alexandrtovmach <alexandrtovmach@gmail.com>
2019-01-11 15:05:27 +0200

    refactored rent adv

--- src/components/RentAdvertise/RentAdvertise.js
+++ src/components/RentAdvertise/RentAdvertise.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Segment, Header, Button, Modal, Image, Grid, List, Card, Icon, Label } from "semantic-ui-react";
+import { Segment, Header, Button, Modal, Image, Grid, List, Card, Icon, Label, Item } from "semantic-ui-react";
 
 import { benefits } from '../../services/constants';
 
@@ -68,94 +68,96 @@ export default class RentAdvertise extends React.Component {
               content={description || "Description is empty..."}
             />
           </Header>
+          {
+            location &&
+            <Modal
+              header="Location of this rent"
+              content="In development"
+              trigger={(
+                <List.Item>
+                  <Segment
+                    as={Button}
+                    fluid
+                    basic
+                  >
+                    <Icon name="map marker alternate"/>
+                    {(location && location.address) || "Look on map"}
+                  </Segment>
+                </List.Item>
+              )}
+              closeOnEscape={true}
+              closeOnDimmerClick={true}
+              onClose={this.toggleAvatarChange}
+              size="small"
+            />
+          }
+          {
+            apartmentsType && rentType &&
+              <Segment
+                basic
+                key={`info-${id}`}
+                color='blue'
+                textAlign="center"
+              >
 
-          <List>
-            {
-              apartmentsType && rentType &&
-              <List.Item>
-                <Label
-                  basic
-                  key={`info-${id}`}
-                  color='blue'
-                >
-                  <Icon name="hotel"/>
-                  {apartmentsType}
-                  &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
-                  <Icon name="clock"/>
-                  {rentType}
-                </Label>
-              </List.Item>
-            }
-            {
-              price &&
-              <List.Item>
-                <Label as='a' color="red">
-                  {price}$
-                </Label>
-              </List.Item>
-            }
-            {
-              startDate &&
-              <List.Item>
-                <List.Content>Available from: {new Date(startDate).toLocaleDateString()}</List.Content>
-              </List.Item>
-            }
-            {
-              endDate &&
-              <List.Item>
-                <List.Content>Available to: {unlimitedDate? "unlimited": new Date(endDate).toLocaleDateString()}</List.Content>
-              </List.Item>
-            }
-            {
-              benefitList &&
-              <List.Item>
-                <List.Content>Benefits: {benefitList.map(val => {
-                  const { icon, text } = (benefits.find(b => b.value === val) || {});
-                  return (
-                    <Icon
-                      key={val}
-                      name={icon}
-                      title={text}
-                    />
-                  )
-                })}
-                </List.Content>
-              </List.Item>
-            }
-            {
-              location &&
-              <Modal
-                header="Location of this rent"
-                content="In development"
-                trigger={(
-                  <List.Item>
-                    <Segment
-                      as={Button}
-                      fluid
-                      basic
-                    >
-                      <Icon name="map marker alternate"/>
-                      {(location && location.address) || "Look on map"}
-                    </Segment>
-                  </List.Item>
-                )}
-                closeOnEscape={true}
-                closeOnDimmerClick={true}
-                onClose={this.toggleAvatarChange}
-                size="small"
-              />
-            }
-          </List>
+                <Item>
+                  <Label color="grey" basic size="large">
+                    {new Date(startDate).toLocaleDateString()}
+                    &nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;
+                    {unlimitedDate? "unlimited": new Date(endDate).toLocaleDateString()}
+                  </Label>
+                </Item>
+                <Item className="margin-v-1">
+                  <Label color="grey" basic size="large">
+                    <Icon name="hotel"/>
+                    {apartmentsType}
+                    &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                    <Icon name="clock"/>
+                    {rentType}
+                  </Label>
+                </Item>
+                <Item>
+                  {
+                    benefitList &&
+                    <Label color="grey" basic>
+                      {benefitList.map(val => {
+                        const { icon, text } = (benefits.find(b => b.value === val) || {});
+                        return (
+                          <>
+                            &nbsp;
+                            <Icon
+                              fitted
+                              key={val}
+                              name={icon}
+                              title={text}
+                              size="large"
+                              color="grey"
+                            />
+                            &nbsp;&nbsp;
+                          </>
+                        )
+                      })}
+                    </Label>
+                  }
+                </Item>
+              </Segment>
+          }
 
           <Modal
             header="Contact with owner?"
             content="In development"
             trigger={(
               <Button
-                primary
                 fluid
+                primary
+                animated="vertical"
               >
-                Send message
+                <Button.Content hidden>
+                  Contact with owner
+                </Button.Content>
+                <Button.Content visible>
+                  {price}$
+                </Button.Content>
               </Button>
             )}
             closeOnEscape={true}
